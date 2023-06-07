624ad935d1ad5a6239beee2a07b00e9f3e25c09a
이승환 <lsh01087674117@gmail.com>
2023-06-07 21:13:50 +0900

    mysql-r2dbc dependencies added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.1.0"
+    id("com.google.devtools.ksp") version "1.8.21-1.0.11"
 
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
@@ -20,6 +21,18 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    val komapperVersion = "1.10.0"
+    platform("org.komapper:komapper-platform:$komapperVersion").let {
+        implementation(it)
+        ksp(it)
+    }
+    implementation("org.komapper:komapper-starter-jdbc")
+    implementation("org.komapper:komapper-dialect-h2-jdbc")
+    ksp("org.komapper:komapper-processor")
+
+    implementation("org.komapper:komapper-dialect-mysql-r2dbc:1.1.2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
