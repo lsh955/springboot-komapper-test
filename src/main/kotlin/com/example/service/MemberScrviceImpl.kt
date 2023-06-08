d7534271a6905efbfc67cc617aa06c46f5456975
이승환 <lsh01087674117@gmail.com>
2023-06-08 23:45:11 +0900

    lists 비즈니스로직 작성.

--- src/main/kotlin/com/example/service/MemberScrviceImpl.kt
+++ src/main/kotlin/com/example/service/MemberScrviceImpl.kt
@@ -1,6 +1,9 @@
 package com.example.service
 
 import com.example.dto.MemberDto
+import com.example.entity.member
+import org.komapper.core.dsl.Meta
+import org.komapper.core.dsl.QueryDsl
 import org.komapper.r2dbc.R2dbcDatabase
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
@@ -10,12 +13,23 @@ class MemberScrviceImpl (
     private val database: R2dbcDatabase,
     private val modelMapper: ModelMapper,
 ): MemberScrvice {
+
+    private val memberMeta = Meta.member
+
     override suspend fun save(event: MemberDto): Boolean {
         TODO("Not yet implemented")
     }
 
-    override suspend fun lists(event: MemberDto): List<MemberDto> {
-        TODO("Not yet implemented")
+    /**
+     * 전체 불러오기.
+     */
+    override suspend fun lists(): List<MemberDto> {
+
+        return database.runQuery {
+            QueryDsl.from(memberMeta)
+        }.map {
+            modelMapper.map(it, MemberDto::class.java)
+        }
     }
 
     override suspend fun update(event: MemberDto): Boolean {
