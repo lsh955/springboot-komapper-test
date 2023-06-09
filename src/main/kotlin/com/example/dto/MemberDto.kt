907eb9b99ff9297608633cfc2db0684cb077273e
seunghwan <lshk955@naver.com>
2023-06-09 14:02:11 +0900

    필요없는 import 삭제.

--- src/main/kotlin/com/example/dto/MemberDto.kt
+++ src/main/kotlin/com/example/dto/MemberDto.kt
@@ -1,5 +1,7 @@
 package com.example.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 
 data class MemberDto (
