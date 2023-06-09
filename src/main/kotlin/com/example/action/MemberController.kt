82bd2df1ccd6feae4813e74f6f65ebef37c8368a
seunghwan <lshk955@naver.com>
2023-06-09 15:24:35 +0900

    오타수정.

--- src/main/kotlin/com/example/action/MemberController.kt
+++ src/main/kotlin/com/example/action/MemberController.kt
@@ -1,23 +1,23 @@
 package com.example.action
 
 import com.example.dto.MemberDto
-import com.example.service.MemberScrvice
+import com.example.service.MemberService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/member")
 class MemberController(
-    private val memberScrvice: MemberScrvice
+    private val memberService: MemberService
 ) {
     @PostMapping("/save")
-    suspend fun save(@RequestBody memberDto: MemberDto)= mapOf("state" to memberScrvice.save(memberDto))
+    suspend fun save(@RequestBody memberDto: MemberDto)= mapOf("state" to memberService.save(memberDto))
 
     @GetMapping("/list")
-    suspend fun list(@RequestParam offset: Int, @RequestParam limit: Int) = memberScrvice.lists(offset, limit)
+    suspend fun list(@RequestParam offset: Int, @RequestParam limit: Int) = memberService.lists(offset, limit)
 
     @PutMapping("/update")
-    suspend fun update(@RequestBody memberDto: MemberDto) = memberScrvice.update(memberDto)
+    suspend fun update(@RequestBody memberDto: MemberDto) = memberService.update(memberDto)
 
     @DeleteMapping("/delete")
-    suspend fun delete(@RequestBody memberDto: MemberDto) = mapOf("state" to memberScrvice.delete(memberDto.memberIdx))
+    suspend fun delete(@RequestBody memberDto: MemberDto) = mapOf("state" to memberService.delete(memberDto.memberIdx))
 }
