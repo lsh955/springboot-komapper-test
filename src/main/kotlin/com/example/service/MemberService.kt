9c95754c377dcfb7defcb274004608af490510f4
seunghwan <lshk955@naver.com>
2023-06-15 17:42:32 +0900

    MemberService 주석추가.

--- src/main/kotlin/com/example/service/MemberService.kt
+++ src/main/kotlin/com/example/service/MemberService.kt
@@ -3,11 +3,23 @@ package com.example.service
 import com.example.dto.MemberDto
 
 interface MemberService {
+    /**
+     * 회원 저장
+     */
     suspend fun save(event:MemberDto): Boolean
 
+    /**
+     * 회원 불러오기
+     */
     suspend fun lists(offset: Int, limit: Int): List<MemberDto>
 
+    /**
+     * 회원 수정
+     */
     suspend fun update(event: MemberDto): Boolean
 
+    /**
+     * 회원 삭제
+     */
     suspend fun delete(memberIdx: Long): Boolean
 }
